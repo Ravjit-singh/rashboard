8be231d4effe21f207bb71b96607b2ff7988a82c
Ravjit-singh <ravjitsingh4444@gmail.com>
2026-06-23 20:44:10 +0530

    fixed the node js error , happened due to lack of comfig in project config file

---
+++ projects.config.js
@@ -0,0 +1,17 @@
+// projects.config.js
+module.exports = [
+  {
+    id: "skillmax_mk2",
+    name: "Skillmax PWA",
+    databaseType: "supabase",
+    envKey: "SKILLMAX_DB_URL",
+    liveUrl: "https://your-skillmax-url.com"
+  },
+  {
+    id: "rchat_v1",
+    name: "RChat V1",
+    databaseType: "firebase",
+    envKey: "RCHAT_DB_URL",
+    liveUrl: "https://your-rchat-url.com"
+  }
+];
